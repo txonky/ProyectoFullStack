056cf78d3599c13316b18e619e011cd1e333cbae
AlexOrtiz <ortizdiazalex@gmail.com>
2018-06-07 19:23:13 +0200

    Creando objetos .js

---
+++ prueba.js
@@ -0,0 +1,60 @@
+let usuario = {
+  usuario: "Alex",
+  activado: "true",
+  tokenRegistro:"12345",
+  baneado: "false",
+  fechaNacimiento: new Date("1985-03-2"),
+  email: "ortizdiazalex@gmail.com",
+  prefijo: "+34",
+  telefono:"665860628",
+  tipo: "admin",
+  dni: "53370788R",
+  referalId:"Ramiro",
+  _id:"",
+  foto: "",
+  loginIp:"",
+  fechaUltimoLogin: new Date(),
+  ventas:[],
+  compras:[],
+  recibidos:[],
+  enviados:[],
+  fondos:[],
+  realcash:1000
+
+}
+
+let moneda1 = {
+  id : "",
+  symbol :"BTC",
+  historico :[{
+    precioUltimo:1.200,
+    precioApertura:0.995,
+    precioCierre:0.700,
+    fechaUltimaActualizacion:new Date(),
+    volumen:234444444,
+    precioMedio:1.45
+  }],
+  conversion : "USDT",
+
+  mediasMoviles:[],
+  osciladores:[]
+
+
+}
+
+let moneda2 = {
+  id :"",
+  symbol: "WTCBTC",
+  historico :[{
+    precioUltimo:1.00,
+    precioApertura:0.755,
+    precioCierre:0.982,
+    fechaUltimaActualizacion:new Date(),
+    volumen:934444444,
+    precioMedio:1.75
+  }],
+  conversion: "BTC",
+  mediasMoviles:[],
+  osciladores:[]
+
+}
